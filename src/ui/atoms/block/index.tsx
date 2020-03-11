6c71d30d6ccf797de28e9bd76a672bd59625e7ee
Sergey Sova <s.sova@redmadrobot.com>
2020-03-11 16:06:48 +0300

    fix(atoms/block): component should be selectable by styled

--- src/ui/atoms/block/index.tsx
+++ src/ui/atoms/block/index.tsx
@@ -1,13 +1,6 @@
-import * as React from 'react';
 import styled from 'styled-components';
 
-type BlockProps = { className?: string };
-
-export const Block: React.FC<BlockProps> = ({ className, children }) => (
-  <WolyBlock className={className}>{children}</WolyBlock>
-);
-
-const WolyBlock = styled.div`
+export const Block = styled.div`
   padding: var(--block-padding);
   border-radius: var(--block-border-radius);
   box-shadow: var(--block-shadow);
