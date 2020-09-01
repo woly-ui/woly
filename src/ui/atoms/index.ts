ad10b5efd0c0756f49778c36b9a38638c3ce81cf
Sergey Sova <mail@sergeysova.com>
2020-09-01 13:12:49 +0300

    feat(atoms): export buttonIcon

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,5 +1,6 @@
 export * as button from './button';
 export * as buttonFloating from './button-floating';
+export * as buttonIcon from './button-icon';
 export * as chip from './chip';
 export * as input from './input';
 export * as label from './label';
