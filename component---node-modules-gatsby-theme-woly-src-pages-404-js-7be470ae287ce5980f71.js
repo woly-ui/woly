71dd0ee6763999b31f0a1a55e3d75f833febfe88
sergeysova <5620073+sergeysova@users.noreply.github.com>
2020-09-08 10:55:38 +0000

    Deploying to gh-pages from  @ 28bb6e417a282717ca8e4e622b4698b10847320c 🚀

---
+++ component---node-modules-gatsby-theme-woly-src-pages-404-js-7be470ae287ce5980f71.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{GZrc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return r}));var u=e("q1tI"),o=e.n(u);function r(){return o.a.createElement("div",null,"Page Not Found")}}}]);
+//# sourceMappingURL=component---node-modules-gatsby-theme-woly-src-pages-404-js-7be470ae287ce5980f71.js.map
