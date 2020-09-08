4e660430a0d3b8d91f25e5ccec50e24fe18a727b
Sergey Sova <mail@sergeysova.com>
2020-09-08 13:21:35 +0300

    docs: remove storybook configs, leave story temporary

--- .storybook/layout/template.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import styled from 'styled-components';
-
-export const Wrap = ({ children }) => <Wrapper>{children}</Wrapper>;
-
-const Wrapper = styled.div`
-  padding: 24px;
-`;
