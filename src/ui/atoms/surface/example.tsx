3068ee3a26a05470eeb9bc7b3528bde2c2a3e2d9
Tatiana Cherednichenko <tatinacher@gmail.com>
2020-12-24 19:13:32 +0700

    feat(ui): add variant prop to Surface

--- src/ui/atoms/surface/example.tsx
+++ src/ui/atoms/surface/example.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import { Button, Popover, Surface } from '..';
+import { Global } from '../../../lib/box-styles';
 
 export const SurfaceExample: React.FC = () => {
   const [isOpen, onClick] = React.useReducer((is) => !is, false);
 
   return (
-    <>
-      <PopoverPrimary isOpen={isOpen}>
+    <Global>
+      <Popover isOpen={isOpen} variant="primary">
         <Surface>
           <Content>
             This is surface
@@ -17,9 +18,9 @@ export const SurfaceExample: React.FC = () => {
             </ButtonWrapper>
           </Content>
         </Surface>
-      </PopoverPrimary>
-      <Button text="show surface" onClick={onClick} />
-    </>
+      </Popover>
+      <Button text="show surface" onClick={onClick} variant="primary" />
+    </Global>
   );
 };
 
@@ -27,22 +28,8 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  --const-m: 6px;
-  --main-level: 3;
 `;
 export const ButtonWrapper = styled.div`
   max-width: 300px;
   padding-top: 30px;
-  --primary-bg: #6ab547;
-  --component-level: 2;
-`;
-
-const PopoverPrimary = styled(Popover)`
-  --popover-position-top: 0;
-  --popover-position-right: 0;
-  --popover-position-left: 0;
-  --popover-position-bottom: 0;
-  --popover-background: #00000063;
-  --popover-spacing-vertical: 300px;
-  --popover-spacing-horizontal: 400px;
 `;
