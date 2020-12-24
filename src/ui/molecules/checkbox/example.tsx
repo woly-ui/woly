ee40d758dde27246024f478c034b93caa139d566
Tatiana Cherednichenko <tatinacher@gmail.com>
2020-12-24 21:38:45 +0700

    feat(ui): add variant prop to Checkbox

--- src/ui/molecules/checkbox/example.tsx
+++ src/ui/molecules/checkbox/example.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import { Checkbox, Label } from '../..';
+import { Global } from '../../../lib/box-styles';
 
 export const CheckboxExample: React.FC = () => {
   const [isPassportChecked, onPassportClick] = React.useReducer(
@@ -15,29 +16,29 @@ export const CheckboxExample: React.FC = () => {
   );
 
   return (
-    <>
+    <Global>
       Travel packing checklist:
       <Checkboxes>
-        <CheckboxPrimary
+        <Checkbox
           label={<Label text="passport" />}
           isChecked={isPassportChecked}
           onChange={onPassportClick}
           id="checkbox"
         />
-        <CheckboxPrimary
+        <Checkbox
           label={<Label text="money" />}
           isChecked={isMoneyChecked}
           onChange={onMoneyClick}
           id="checkbox"
         />
-        <CheckboxPrimary
+        <Checkbox
           label={<Label text="tickets" />}
           isChecked={isTicketsChecked}
           onChange={onTicketsClick}
           id="checkbox"
         />
       </Checkboxes>
-    </>
+    </Global>
   );
 };
 
@@ -49,16 +50,4 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  --const-m: 6px;
-  --main-level: 3;
-`;
-export const ButtonWrapper = styled.div`
-  max-width: 300px;
-  padding-top: 30px;
-  --primary-bg: #6ab547;
-  --component-level: 2;
-`;
-
-const CheckboxPrimary = styled(Checkbox)`
-  --padding: 5px;
 `;
