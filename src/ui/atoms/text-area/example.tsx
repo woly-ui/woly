556fd60f83dffe2e9bd30e3c94cf37153d50daf2
Ira <i.aristova@opends.tech>
2021-03-19 13:21:53 +0300

    feat : add props and variants to textarea

--- src/ui/atoms/text-area/example.tsx
+++ src/ui/atoms/text-area/example.tsx
@@ -1,77 +1,132 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Global, block } from 'box-styles';
-import { TextArea } from 'ui';
+import { Label, TextArea } from 'ui';
 
-export const TextAreaExample: React.FC = () => (
-  <form autoComplete="off">
+export const TextAreaExample: React.FC = () => {
+  const [valueL, setValueL] = React.useState('');
+  const [valueN, setValueN] = React.useState('');
+  const [valueE, setValueE] = React.useState('');
+
+  const textareaRef = React.useRef<any>(null);
+  const [currentValue, setCurrentValue] = React.useState('');
+
+  React.useEffect(() => {
+    if (textareaRef && textareaRef.current) {
+      textareaRef.current.style.height = '0px';
+      const scrollHeight = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = scrollHeight + 'px';
+    }
+  }, [currentValue]);
+
+  return (
     <Global>
       <TextAreaWrappers>
         <TextAreas>
-          <span>Large</span>
+          <p>Large</p>
           <block.L>
             <TextArea
+              label={<Label text="Textarea without resize and overflow"/>}
               name="name"
               placeholder="Disabled"
               onChange={() => console.info('On textarea change')}
               variant="base"
               disabled
+              resize="none"
+              overflow="hidden"
             />
             <TextArea
+              label={<Label text="Textarea with maxLength"/>}
               name="name"
+              className="resize"
               placeholder="Default"
-              onChange={() => console.info('On textarea change')}
+              onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setValueL(event.target.value)
+              }
               variant="base"
+              maxLength={100}
+              value={valueL}
+              overflow="auto"
             />
             <TextArea
+              label={<Label text="Textarea with maxLength and errortext"/>}
               name="name"
               placeholder="Error"
-              onChange={() => console.info('On textarea change')}
+              onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setValueE(event.target.value)
+              }
               variant="error"
+              maxLength={100}
+              value={valueE}
+              textError="Error text"
+              overflow="hidden"
             />
           </block.L>
         </TextAreas>
         <TextAreas>
-          <span>Normal</span>
+          <p>Normal</p>
           <block.M>
             <TextArea
+              label={<Label text="Label content"/>}
               name="name"
               placeholder="Disabled"
               onChange={() => console.info('On textarea change')}
               variant="base"
               disabled
+              resize="none"
+              overflow="hidden"
             />
             <TextArea
+              label={<Label text="Textarea with colms, rows and overflowX"/>}
               name="name"
               placeholder="Default"
-              onChange={() => console.info('On textarea change')}
+              onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setValueN(event.target.value)
+              }
               variant="base"
+              value={valueN}
+              maxLength={50}
+              rows={2}
+              cols={20}
+              wrap="off"
+              overflow="auto"
             />
             <TextArea
+              label={<Label text="Label content"/>}
               name="name"
               placeholder="Error"
               onChange={() => console.info('On textarea change')}
               variant="error"
+              textError="Error text"
             />
           </block.M>
         </TextAreas>
         <TextAreas>
-          <span>Small</span>
+          <p>Small</p>
           <block.S>
             <TextArea
+              label={<Label text="Simple disabled without resize"/>}
               name="name"
               placeholder="Disabled"
               onChange={() => console.info('On textarea change')}
               variant="base"
               disabled
+              resize="none"
             />
             <TextArea
+              label={<Label text="Simple textarea"/>}
               name="name"
+              onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                setCurrentValue(event.target.value);
+              }}
               placeholder="Default"
-              onChange={() => console.info('On textarea change')}
-              variant="base"
+              ref={textareaRef}
+              value={currentValue}
+              resize="auto"
+              overflow="hidden"
             />
             <TextArea
+              label={<Label text="Textarea without error"/>}
               name="name"
               placeholder="Error"
               onChange={() => console.info('On textarea change')}
@@ -81,8 +136,8 @@ export const TextAreaExample: React.FC = () => (
         </TextAreas>
       </TextAreaWrappers>
     </Global>
-  </form>
-);
+  );
+};
 
 export const TextAreas = styled.div`
   max-width: 300px;
@@ -96,9 +151,9 @@ export const TextAreas = styled.div`
   ${block.L} > ${TextArea} {
     margin: 0 0 36px 0;
   }
-  span {
+
+  p {
     display: block;
-    padding-bottom: 20px;
   }
 `;
 export const TextAreaWrappers = styled.div`
--- src/ui/atoms/text-area/index.tsx
+++ src/ui/atoms/text-area/index.tsx
@@ -15,55 +15,133 @@ import { Variant } from 'lib/types';
  * --woly-color-disabled
  *
  */
-
 interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+  cols?: number;
+  disabled?: boolean;
+  label?: React.ReactNode;
+  maxLength?: number;
+  minlength?: number;
   name: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => unknown;
-  className?: string;
-  disabled?: boolean;
   onKeyDown?: (event: React.KeyboardEvent<HTMLTextAreaElement>) => unknown;
+  overflow?: string;
   placeholder?: string;
+  resize?: string;
   rows?: number;
+  textError?: string;
+  textareaRef?: any;
+  value?: string;
+  wrap?: string;
 }
 
 const TextAreaBase: React.FC<TextAreaProps & Variant> = ({
   className,
+  cols,
+  disabled,
+  label,
+  maxLength,
+  minlength,
   name,
   onChange,
-  variant = 'default',
-  disabled,
   onKeyDown,
+  overflow,
   placeholder,
+  resize,
   rows,
+  textError,
+  textareaRef,
+  value,
+  variant = 'default',
+  wrap,
   ...p
-}) => (
-  <textarea
-    className={className}
-    data-variant={variant}
-    name={name}
-    onChange={onChange}
-    disabled={disabled}
-    onKeyDown={onKeyDown}
-    placeholder={placeholder}
-    rows={rows}
-    {...p}
-  />
-);
+}) => {
+  return (
+    <TextAreaWrapper
+      data-overflow={overflow}
+      data-resize={resize}
+      data-variant={variant}
+     >
+      {label && <span>{label}</span>}
+      <textarea
+        className={className}
+        cols={cols}
+        data-variant={variant}
+        disabled={disabled}
+        maxLength={maxLength}
+        minLength={minlength}
+        name={name}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        placeholder={placeholder}
+        ref={textareaRef}
+        rows={rows}
+        value={value}
+        wrap={wrap}
+        {...p}
+      />
+      <Containrer>
+        {textError && <span>{textError}</span>}
+        {maxLength && (
+          <p>
+            {value?.length}/{maxLength}
+          </p>
+        )}
+      </Containrer>
+    </TextAreaWrapper>
+  );
+};
+
+export const TextAreaWrapper = styled.div`
+  --woly-vertical: calc(
+    1px * var(--woly-component-level) * var(--woly-main-level)
+  );
+
+  --woly-width: 100%;
+  width: var(--woly-width);
+
+  padding: var(--woly-vertical, 16px) 0;
+`;
+
+export const Containrer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  p {
+    margin: 0;
+
+    font-size: var(--woly-font-size, 12px);
+    line-height: var(--woly-line-height, 15px);
+
+    color: var(--woly-count-color, #000000);
+  }
+
+  span {
+    flex-grow: 1;
+
+    font-size: var(--woly-font-size, 12px);
+    line-height: var(--woly-line-height, 15px);
+
+    color: var(--woly-error-text, #000000);
+  }
+`;
 
 export const TextArea = styled(TextAreaBase)`
-  --woly-textarea-width: 100%;
   --woly-vertical: calc(
     1px * var(--woly-component-level) * var(--woly-main-level)
   );
   --woly-horizontal: calc(
     var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--woly-vertical)
   );
-  box-sizing: border-box;
-  width: var(--woly-textarea-width);
+
   padding: var(--woly-vertical, 16px) var(--woly-horizontal, 6.4px);
-  resize: both;
+  resize: ${(p) => p.resize};
+
   color: var(--woly-color, #000000);
+
+  overflow: ${(p) => p.overflow};
+
   font-size: var(--woly-font-size, 16px);
   line-height: var(--woly-line-height, 24px);
   background: var(--woly-background, transparent);
@@ -78,10 +156,13 @@ export const TextArea = styled(TextAreaBase)`
     font-size: var(--woly-font-size, 16px);
     padding: var(--woly-vertical, 16px) var(--woly-horizontal, 6.4px);
   }
-  &:focus {
+
+  &:focus,
+  &:active {
     border-color: var(--woly-border-focus, #000000);
     outline: none;
   }
+
   &:disabled {
     color: var(--woly-color-disabled, #ffffff);
     background: var(--woly-background-disabled, #ffffff);
@@ -90,8 +171,4 @@ export const TextArea = styled(TextAreaBase)`
       var(--woly-background-disabled, #000000)
     );
   }
-` as StyledComponent<
-  'textarea',
-  Record<string, unknown>,
-  TextAreaProps & Variant
->;
+` as StyledComponent<'textarea',Record<string, unknown>,TextAreaProps & Variant>;
