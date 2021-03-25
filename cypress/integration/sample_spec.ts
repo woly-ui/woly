1a16c97c2c63f762bdd582dbb9868046e1a7fc4c
Sergey Sova <mail@sergeysova.com>
2021-03-25 22:33:55 +0300

    test: setup percy with property testing

--- cypress/integration/sample_spec.ts
+++ cypress/integration/sample_spec.ts
@@ -3,3 +3,11 @@ describe('my first test', () => {
     expect(true).to.equal(true);
   });
 });
+
+describe('first test', () => {
+  it('button', () => {
+    cy.visit('/package/woly/component/button');
+    cy.get('[data-cypress="button"]', { timeout: 5000 }).should('be.visible');
+    cy.percySnapshot('button');
+  });
+});
--- src/ui/atoms/button/example.tsx
+++ src/ui/atoms/button/example.tsx
@@ -4,82 +4,19 @@ import { ArrowLeft, SearchIcon } from 'icons';
 import { Button } from 'ui';
 import { Global, block } from 'box-styles';
 
-export const ButtonExample: React.FC = () => (
-  <Global>
-    <Buttons>
-      <block.L>
-        <span>Large</span>
+const properties = {
+  size: ['large', 'normal', 'small'],
+  variant: ['primary', 'secondary'],
+  icon: ['no', 'left', 'right'],
+};
 
-        <Button variant="primary" text="Button" />
-        <Button variant="secondary" text="Button" />
-        <Button variant="primary" text="Button" disabled />
-      </block.L>
-      <block.M>
-        <span>Normal</span>
-        <Button
-          variant="primary"
-          text="Button"
-          right={
-            <IconWrapper>
-              <SearchIcon />
-            </IconWrapper>
-          }
-        />
-        <Button
-          variant="secondary"
-          text="Button"
-          right={
-            <IconSecondaryWrapper>
-              <SearchIcon />
-            </IconSecondaryWrapper>
-          }
-        />
-        <Button
-          variant="primary"
-          text="Button"
-          right={
-            <IconDisabledWrapper>
-              <SearchIcon />
-            </IconDisabledWrapper>
-          }
-          disabled
-        />
-      </block.M>
-      <block.S>
-        <span>Small</span>
-        <Button
-          variant="primary"
-          text="Button"
-          left={
-            <IconWrapper>
-              <ArrowLeft />
-            </IconWrapper>
-          }
-        />
-        <Button
-          variant="secondary"
-          text="Button"
-          left={
-            <IconSecondaryWrapper>
-              <ArrowLeft />
-            </IconSecondaryWrapper>
-          }
-        />
-        <Button
-          variant="primary"
-          text="Button"
-          disabled
-          left={
-            <IconDisabledWrapper>
-              <ArrowLeft />
-            </IconDisabledWrapper>
-          }
-        />
-      </block.S>
-    </Buttons>
-  </Global>
-);
-export const IconWrapper = styled.div`
+const wrapperMatch: any = {
+  large: block.L,
+  normal: block.M,
+  small: block.S,
+};
+
+const IconWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -89,40 +26,160 @@ export const IconWrapper = styled.div`
   }
 `;
 
-export const IconSecondaryWrapper = styled(IconWrapper)`
+const IconSecondaryWrapper = styled(IconWrapper)`
   path {
     fill: #683aef;
   }
 `;
 
+const iconWrapper: any = {
+  primary: IconWrapper,
+  secondary: IconSecondaryWrapper,
+};
+
+const renderItem = ({ size, variant, icon }: any) => {
+  const Wrapper = wrapperMatch[size];
+  const props: any = {
+    variant,
+    text: `Sample`,
+  };
+  const Icon = iconWrapper[variant];
+
+  if (icon === 'left') {
+    props.left = (
+      <Icon>
+        <ArrowLeft />
+      </Icon>
+    );
+  }
+  if (icon === 'right') {
+    props.right = (
+      <Icon>
+        <SearchIcon />
+      </Icon>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <Button {...props} />
+    </Wrapper>
+  );
+};
+
+const renderSize = ({ size, variant, icon }: any) => {
+  const label = <span>{size}</span>;
+  const items = [];
+  for (const a of icon) {
+    for (const b of variant) {
+      for (const c of size) {
+        items.push(renderItem({ size: c, variant: b, icon: a }));
+      }
+    }
+  }
+  return <>{items}</>;
+};
+
+export const ButtonExample: React.FC = () => (
+  <>
+    <Global>
+      <div data-cypress="button">
+        <Grid
+          style={{
+            gridTemplateColumns: `repeat(${properties.size.length}, auto)`,
+          }}
+        >
+          {renderSize(properties)}
+        </Grid>
+      </div>
+    </Global>
+  </>
+);
+
+export const ButtonExample_: React.FC = () => (
+  <Global>
+    <div data-cypress="button">
+      <Grid>
+        <block.L>
+          <span>Large</span>
+
+          <Button variant="primary" text="Button" />
+          <Button variant="secondary" text="Button" />
+          <Button variant="primary" text="Button" disabled />
+        </block.L>
+        <block.M>
+          <span>Normal</span>
+          <Button
+            variant="primary"
+            text="Button"
+            right={
+              <IconWrapper>
+                <SearchIcon />
+              </IconWrapper>
+            }
+          />
+          <Button
+            variant="secondary"
+            text="Button"
+            right={
+              <IconSecondaryWrapper>
+                <SearchIcon />
+              </IconSecondaryWrapper>
+            }
+          />
+          <Button
+            variant="primary"
+            text="Button"
+            right={
+              <IconDisabledWrapper>
+                <SearchIcon />
+              </IconDisabledWrapper>
+            }
+            disabled
+          />
+        </block.M>
+        <block.S>
+          <span>Small</span>
+          <Button
+            variant="primary"
+            text="Button"
+            left={
+              <IconWrapper>
+                <ArrowLeft />
+              </IconWrapper>
+            }
+          />
+          <Button
+            variant="secondary"
+            text="Button"
+            left={
+              <IconSecondaryWrapper>
+                <ArrowLeft />
+              </IconSecondaryWrapper>
+            }
+          />
+          <Button
+            variant="primary"
+            text="Button"
+            disabled
+            left={
+              <IconDisabledWrapper>
+                <ArrowLeft />
+              </IconDisabledWrapper>
+            }
+          />
+        </block.S>
+      </Grid>
+    </div>
+  </Global>
+);
 export const IconDisabledWrapper = styled(IconWrapper)`
   path {
     fill: #a39bb2;
   }
 `;
 
-export const Buttons = styled.div`
-  display: flex;
-
-  ${block.L} > button {
-    margin-bottom: 30px;
-  }
-
-  ${block.M} > button {
-    margin-bottom: 36px;
-  }
-
-  ${block.S} > button {
-    margin-bottom: 42px;
-  }
-
-  ${block.S} > span, ${block.M} > span, ${block.L} > span {
-    padding-bottom: 20px;
-  }
-
-  & > div {
-    display: flex;
-    flex-direction: column;
-    padding-right: 40px;
-  }
+export const Grid = styled.div`
+  display: grid;
+  grid-gap: 1rem;
 `;
