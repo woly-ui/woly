9cf7fba193505cff1d5f2e9e18fb9af735364b38
tatinacher <tatinacher@gmail.com>
2021-04-07 12:52:04 +0300

    [FRNT-366] fix surface (#64)

--- src/ui/atoms/surface/index.tsx
+++ src/ui/atoms/surface/index.tsx
@@ -17,12 +17,11 @@ const map = (properties: Variant) => ({
 });
 
 export const Surface = styled.div.attrs(map)`
-  padding: var(--woly-padding, 16px);
-
+  padding: var(--woly-padding, 0);
   background-color: var(--woly-surface, #ffffff);
   border-color: var(--woly-border, #000000);
   border-style: solid;
   border-width: var(--woly-border-width, 0);
   border-radius: var(--woly-rounding, 3px);
-  box-shadow: var(--woly-box-shadow, none);
+  box-shadow: var(--woly-box-shadow, 3px 3px 8px rgba(11, 31, 53, 0.04));
 `;
