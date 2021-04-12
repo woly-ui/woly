3ee7f6d3f359af917ef5071c9c22a80bf6db4e2a
Tatiana Cherednichenko <tatinacher@gmail.com>
2021-04-12 12:33:58 +0300

    fix: add missed ui imports

--- src/ui/molecules/index.ts
+++ src/ui/molecules/index.ts
@@ -2,4 +2,5 @@ export { Checkbox } from './checkbox';
 export { Field } from './field';
 export { InputPassword } from './input-password';
 export { Popover } from './popover';
+export { Select } from './select';
 export { Switch } from './switch';
