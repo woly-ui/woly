fa0e4e0f5deb2f804e9d5c4e1129fcb437f7174b
Ira <i.aristova@opends.tech>
2021-04-14 12:39:53 +0300

    fix: fix styles

--- src/ui/molecules/field/index.tsx
+++ src/ui/molecules/field/index.tsx
@@ -4,77 +4,54 @@ import { Variant } from 'lib/types';
 
 import { Label } from '../../atoms';
 
-/**
- * --woly-border
- * --woly-border-focus
- * --woly-border-disabled
- * --woly-border-width
- * --woly-rounding
- *
- * --woly-background
- * --woly-background-diabled
- * --woly-color
- * --woly-color-disabled
- */
-
 interface FieldProps {
   className?: string;
-  isDisabled?: boolean;
-  isRow?: boolean;
+  id?: string;
+  row?: boolean;
   label?: React.ReactNode;
 }
 
-export const FieldBase: React.FC<FieldProps & Variant> = ({
+const FieldBase: React.FC<FieldProps & Variant> = ({
   children,
   className,
-  isDisabled,
-  isRow,
   label,
+  row = false,
   variant = 'default',
-}) => {
-  return (
-    <div
-      className={className}
-      data-disabled={isDisabled}
-      data-row={isRow}
-      data-variant={variant}
-    >
-      <div data-field="label">{label && <Label>{label}</Label>}</div>
-      <div data-field="content">{children}</div>
+}) => (
+  <div className={className} data-row={row} data-variant={variant}>
+    <div data-field="label">
+      <Label>
+        {label}
+        <div data-field="content">{children}</div>
+      </Label>
     </div>
-  );
-};
+  </div>
+);
 
 export const Field = styled(FieldBase)`
-  --woly-vertical: calc(
+  --local-vertical: calc(
     1px * var(--woly-component-level) * var(--woly-main-level)
   );
-  --woly-horizontal: calc(
-    var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--woly-vertical)
+  --local-horizontal: calc(
+    var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--local-vertical)
   );
 
-  padding-bottom: var(--woly-vertical, 6px) 0;
+  padding-bottom: var(--local-vertical, 6px);
 
   box-sizing: border-box;
 
+  width: 100%;
+
   label {
-    padding: var(--woly-vertical, 6px) 0;
+    padding: var(--local-vertical, 6px) 0;
   }
-
-  [data-field='content'] {
-    width: var(--woly-width);
-  }
-
+  
   &[data-row='true'] {
     display: flex;
     align-items: center;
 
     [data-field='label'] {
-      padding: 0 var(--woly-horizontal, 6px);
+      padding: 0 var(--local-horizontal, 6px);
     }
   }
-
-  &:focus-within {
-    border-color: var(--woly-border-focus, #9381f1);
-  }
-` as StyledComponent<'div', Record<string, unknown>, FieldProps & Variant>;
+` as StyledComponent<'label', Record<string, unknown>, FieldProps & Variant>;
