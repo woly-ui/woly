0ec10ec2b46579e66b7246fc87bcb1e71b451ced
Ira <i.aristova@opends.tech>
2021-04-15 21:21:37 +0300

    fix: add examples, fix styles

--- src/ui/atoms/list/index.tsx
+++ src/ui/atoms/list/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled, { StyledComponent } from 'styled-components';
 import { Variant } from 'lib/types';
+import { keyboardEventHandle } from 'lib';
 interface List {
   className?: string;
   list: Array<{
@@ -9,19 +10,51 @@ interface List {
     text: React.ReactNode;
     id: string;
     disabled?: boolean;
-    onClick?: React.MouseEventHandler<HTMLLIElement>;
+    onClick: React.EventHandler<React.SyntheticEvent>;
   }>;
 }
 
-const ListBase: React.FC<List & Variant> = ({ className, list, variant = 'default' }) => (
+const ListBase: React.FC<List & Variant> = ({ className, list, variant = 'default', ...p }) => (
   <ul className={className} data-variant={variant}>
-    {list.map(({ left, right, text, id, disabled = 'false', onClick }) => (
-      <li key={id} data-type="list-item" data-disabled={disabled} onClick={onClick}>
-        {left && <span data-icon="left">{left}</span>}
-        <span data-block="content">{text}</span>
-        {right && <span data-icon="right">{right}</span>}
-      </li>
-    ))}
+    {list.map(({ left, right, text, id, disabled = 'false', onClick }) => {
+      const tabIndex = disabled ? -1 : 0;
+
+      const onKeyDown = React.useCallback(
+        (event: React.KeyboardEvent) => {
+          console.log(1);
+
+          if (event.key === 'Enter') {
+            event.preventDefault();
+          }
+          const keyHandler = {
+            enter: (event: React.SyntheticEvent<Element, Event>) => {
+              onClick(event);
+            },
+          };
+
+          keyboardEventHandle({
+            event,
+            keyHandler,
+          });
+        },
+        [onClick],
+      );
+
+      return (
+        <li
+          data-disabled={disabled}
+          data-type="list-item"
+          key={id}
+          onClick={onClick}
+          onKeyDown={onKeyDown}
+          tabIndex={tabIndex}
+        >
+          {left && <span data-icon="left">{left}</span>}
+          <span data-text="text">{text}</span>
+          {right && <span data-icon="right">{right}</span>}
+        </li>
+      );
+    })}
   </ul>
 );
 
@@ -33,71 +66,81 @@ export const List = styled(ListBase)`
 
   --local-gap: var(--local-vertical);
   --local-compensate: var(--woly-const-m);
+  --local-margin: var(--woly-border-width);
 
   --local-color: var(--woly-canvas-text-default);
   --local-background: var(--woly-shape-text-default);
   --local-item-background: var(--woly-canvas-default);
+  --local-border: var(--woly-canvas-default);
 
   box-sizing: border-box;
+
   width: 100%;
 
   padding: 0;
+  margin: 0;
+
+  border: var(--woly-border-width) solid var(--local-border);
 
   list-style-type: none;
 
-  [data-icon] {
-    --local-icon-size: var(--woly-line-height);
-    display: flex;
-    flex-shrink: 0;
-    align-items: center;
-    justify-content: center;
-
-    width: var(--local-icon-size);
-    height: var(--local-icon-size);
-    padding-right: 6px;
-
-    svg > path {
-      fill: var(--local-color);
-    }
-  }
-
   li[data-type='list-item'] {
     display: flex;
-    flex: 1;
     align-items: center;
 
-    padding: var(--local-vertical) var(--local-horizontal);
-
-    background: var(--local-item-background);
+    padding: var(--local-vertical) 0;
+    margin-bottom: var(--local-margin);
 
     font-size: var(--woly-font-size, 15px);
     line-height: var(--woly-line-height, 24px);
 
     color: var(--local-color);
+    background: var(--local-item-background);
 
     cursor: pointer;
-
     outline: none;
-    border-radius: var(--woly-rounding);
 
-    [data-block='content'] {
+    [data-text] {
+      display: flex;
       flex: 1;
+      padding: 0 var(--local-horizontal);
+    }
+
+    [data-icon] {
+      --local-icon-size: var(--woly-line-height);
+      display: flex;
+      flex-shrink: 0;
+      align-items: center;
+      justify-content: center;
+      padding: 0 calc(var(--local-horizontal) - var(--local-compensate));
+
+      width: var(--local-icon-size);
+      height: var(--local-icon-size);
+
+      svg > path {
+        fill: var(--local-color);
+      }
+    }
+
+    [data-icon='left'] ~ [data-text],
+    [data-text] ~ [data-icon='right'] {
+      padding-left: var(--local-gap);
     }
 
     &:hover {
       --local-item-background: var(--woly-canvas-disabled);
     }
-    &:focus{
+    &:focus {
       box-shadow: 0 0 0 1.5px var(--woly-focus);
     }
     &:active {
-     --local-item-background: var(--woly-focus);
-     --local-color: var( --woly-shape-text-active);
+      --local-item-background: var(--woly-focus);
+      --local-color: var(--woly-shape-text-active);
     }
 
     &[data-disabled='true'] {
       --local-color: var(--woly-canvas-text-disabled);
-      
+
       pointer-events: none;
 
       [data-icon] {
