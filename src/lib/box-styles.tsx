9f3da585411ff56646458c6fe19b58ac1fa3d044
Tatiana Cherednichenko <tatinacher@gmail.com>
2021-04-15 15:14:01 +0300

    fix: fix primary variant

--- src/lib/box-styles.tsx
+++ src/lib/box-styles.tsx
@@ -27,13 +27,13 @@ export const Global = styled.div`
     --woly-shape-text-hover: var(--palette-snow-0);
     --woly-shape-text-active: var(--palette-snow-0);
     --woly-canvas-default: transparent;
-    --woly-canvas-disabled: var(--palete-snow-100);
+    --woly-canvas-disabled: var(--palette-snow-100);
     --woly-canvas-hover: var(--palette-snow-500);
     --woly-canvas-active: var(--palette-snow-500);
     --woly-canvas-text-default: var(--palette-snow-1000);
-    --woly-canvas-text-disabled: var(--woly-snow-500);
-    --woly-canvas-text-hover: var(--woly-snow-500);
-    --woly-canvas-text-active: var(--woly-snow-500);
+    --woly-canvas-text-disabled: var(--palette-snow-500);
+    --woly-canvas-text-hover: var(--palette-snow-500);
+    --woly-canvas-text-active: var(--palette-snow-500);
   }
   [data-variant='secondary'] {
     --woly-border-width: 2px;
