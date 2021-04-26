8978c072a7b5b904fdf0798ac2936cf4deaaecf2
Tatiana Cherednichenko <tatinacher@gmail.com>
2021-04-26 16:26:50 +0300

    fix: add styled-components type

--- src/ui/atoms/header-panel/index.tsx
+++ src/ui/atoms/header-panel/index.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 import { Variant } from 'lib/types';
 
 const map = (properties: Variant) => ({
@@ -18,11 +18,11 @@ export const HeaderPanel = styled.div.attrs(map)`
   );
 
   --local-box-shadow: 3px 3px 9px rgba(57, 57, 57, 0.12);
-  
+
   padding: var(--local-vertical) var(--local-horizontal);
 
   background-color: var(--woly-background);
   border: var(--woly-border-width) solid var(--woly-background);
   border-radius: var(--woly-rounding);
   box-shadow: var(--local-box-shadow);
-`;
+` as StyledComponent<'div', Record<string, unknown>, Variant>;
