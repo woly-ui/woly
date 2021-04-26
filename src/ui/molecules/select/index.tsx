99c91a1feac73d06e58666aa1d116ca0f2a48192
Ira <i.aristova@opends.tech>
2021-04-26 17:03:04 +0300

    rewrite select styles

--- src/ui/molecules/select/index.tsx
+++ src/ui/molecules/select/index.tsx
@@ -2,18 +2,6 @@ import * as React from 'react';
 import styled, { StyledComponent } from 'styled-components';
 import { Variant } from 'lib/types';
 import { keyHandlerGet, keyboardEventHandle } from 'lib';
-
-/**
- * --woly-border
- * --woly-border-focus
- * --woly-border-disabled
- * --woly-border-width
- * --woly-canvas
- * --woly-color-hover
- * --woly-background-hover
- * --woly-line-height
- */
-
 interface SelectOptionProps {
   children: React.ReactNode;
   value: string;
@@ -22,7 +10,7 @@ interface SelectOptionProps {
 interface SelectProps {
   className?: string;
   icon?: React.ReactNode;
-  isDisabled?: boolean;
+  disabled?: boolean;
   onBlur?: React.FocusEventHandler<HTMLElement>;
   onChange: React.EventHandler<React.SyntheticEvent>;
   onFocus?: React.FocusEventHandler<HTMLElement>;
@@ -34,7 +22,7 @@ interface SelectProps {
 export const SelectBase: React.FC<SelectProps & Variant> = ({
   className,
   icon,
-  isDisabled,
+  disabled,
   onBlur,
   onChange,
   onFocus,
@@ -46,7 +34,7 @@ export const SelectBase: React.FC<SelectProps & Variant> = ({
   const [isOpen, setIsOpen] = React.useReducer((is) => !is, false);
   const selectRef = React.useRef(null);
   const dropdownRef = React.useRef(null);
-  const tabIndex = isDisabled ? -1 : 0;
+  const tabIndex = disabled ? -1 : 0;
   const select = selected ?? placeholder;
 
   const onKeyDown = React.useCallback(
@@ -79,7 +67,7 @@ export const SelectBase: React.FC<SelectProps & Variant> = ({
   return (
     <div
       className={className}
-      data-disabled={isDisabled}
+      data-disabled={disabled}
       data-open={isOpen}
       data-select
       data-variant={variant}
@@ -91,7 +79,7 @@ export const SelectBase: React.FC<SelectProps & Variant> = ({
       tabIndex={tabIndex}
     >
       <div data-selected>
-        <div>{select}</div>
+        <div data-value>{select}</div>
         <span data-icon={isOpen}>{icon}</span>
       </div>
       {/* В дальнейшем использовать Popover и List для создания выпадающего списка элементов */}
@@ -113,100 +101,141 @@ export const SelectBase: React.FC<SelectProps & Variant> = ({
 };
 
 export const Select = styled(SelectBase)`
-  --woly-gap: calc(
-    (1px * var(--woly-main-level)) + (1px * var(--woly-main-level) * var(--woly-component-level))
+  --local-vertical: calc(1px * var(--woly-component-level) * var(--woly-main-level));
+  --local-horizontal: calc(
+    var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--local-vertical)
   );
-  --woly-vertical: calc(1px * var(--woly-component-level) * var(--woly-main-level));
-  --woly-horizontal: calc(
-    var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--woly-vertical)
-  );
+
+  --local-gap: var(--woly-const-m);
+
+  --local-background-input-color: var(--woly-canvas-default);
+  --local-border-input-color: var(--woly-canvas-text-active);
+
+  --local-icon-fill: var(--woly-canvas-text-disabled);
+
+  --local-list-background: var(--woly-shape-text-default);
+  --local-border-list: var(--woly-canvas-default);
+
+  --local-value-color: var(--woly-canvas-text-disabled);
+  --local-item-background: var(--woly-canvas-default);
+
+  --local-select-width: 225px;
+
   position: relative;
   align-items: center;
   outline: none;
   cursor: pointer;
-  &:focus > div[data-selected],
-  &:active > div[data-selected] {
-    border-color: var(--woly-background-focus, #1f68f5);
-    color: var(--woly-color-focus, #000000);
-    svg > path {
-      fill: var(--woly-color-focus, #c4c4c4);
-    }
-  }
-  &[data-open='true'] > div[data-selected] {
-    border-color: var(--woly-border-focus, #b0a3f4);
-  }
-  svg > path {
-    fill: var(--woly-color, #000000);
-  }
-  &[data-disabled='true'] {
-    pointer-events: none;
-    svg > path {
-      fill: var(--woly-color-disabled, #c4c4c4);
-    }
-    div[data-selected] {
-      background: var(--woly-background-disabled, #ffffff);
-      border-color: var(--woly-border-disabled, var(--woly-background-disabled, #c4c4c4));
-      color: var(--woly-color-disabled, #c4c4c4);
-    }
-  }
-  div[data-selected] {
+
+  [data-selected] {
     display: flex;
     align-items: center;
-    padding: var(--woly-vertical, 16px) var(--woly-horizontal, 6.4px);
-    background: var(--woly-canvas, #ffffff);
-    border-color: var(--woly-border, var(--woly-background, #f8f7f7));
-    border-style: solid;
-    border-width: var(--woly-border-width, 1.5px);
-    border-radius: var(--woly-rounding, 3px);
+
+    padding: var(--local-vertical) var(--local-horizontal);
+    background: var(--local-background-input-color);
+
+    border: var(--woly-border-width) solid var(--local-border-input-color);
+    border-radius: var(--woly-rounding);
     box-sizing: border-box;
-    color: var(--woly-color, #000000);
-    div {
-      width: 225px;
+
+    color: var(--local-value-color);
+
+    [data-value] {
+      width: var(--local-select-width);
     }
   }
-  span[data-icon='true'] {
+
+  [data-icon='true'] {
     transform: rotate(180deg);
   }
-  span[data-icon='false'] {
+
+  [data-icon] {
     display: flex;
     flex-direction: column;
     justify-content: center;
     height: 100%;
+
+    svg > path {
+      fill: var(--local-icon-fill);
+    }
   }
+
   ul {
     display: none;
-    position: absolute;
-    width: 100%;
     list-style-type: none;
-    background-color: var(--woly-canvas, #ffffff);
-    border-color: var(--woly-border, var(--woly-background, #f8f7f7));
-    border-style: solid;
-    border-width: var(--woly-border-width, 1px);
-    border-radius: var(--woly-rounding, 3px);
-    padding: 0;
+
+    position: absolute;
     z-index: 1;
-    margin-top: 6px;
-    box-shadow: 3px 3px 9px rgba(57, 57, 57, 0.12);
+    width: 100%;
+    
+    background: var(--local-list-background);
+    
+    border: var(--woly-border-width) solid var(--local-border-list);
+    border-radius: var(--woly-rounding);
+    
+    padding: 0;
+    margin-top: var(--local-gap);
   }
+
   ul[data-visible='true'] {
     display: flex;
     flex-direction: column;
+
     box-sizing: border-box;
   }
+
   li {
-    padding: var(--woly-vertical, 16px) var(--woly-horizontal, 6.4px);
+    padding: var(--local-vertical) var(--local-horizontal);
+    
     line-height: var(--woly-line-height, 24px);
-    color: var(--woly-color, #000000);
+    
+    background: var(--local-item-background);
+    color: var(--local-value-color);
+    
     cursor: pointer;
-    &:hover,
+  
+    &:hover {
+      --local-item-background: var(--woly-canvas-disabled);
+    }
+   
     &:focus,
     &:active {
       outline: none;
-      color: var(--woly-color-hover, #1a1a23);
-      background-color: var(--woly-background-hover, #f8f8fa);
+      --local-value-color: var(--woly-canvas-text-default);
     }
   }
-  div:not(:first-child) {
-    margin-top: var(--woly-gap);
+
+  &:focus > div[data-selected],
+  &:active > div[data-selected] {
+    
+    --local-border-color: var(--woly-focus);
+    box-shadow: 0 0 0 var(--woly-border-width) var(--woly-focus);
+
+    --local-value-color: var(--woly-canvas-text-default);
+    
+    [data-icon] {
+       --local-icon-fill: var(--woly-canvas-text-default);
+    }
+  }
+
+  &[data-open='true'] > div[data-selected] {
+    border-color: var(--woly-focus);
+
+    svg > path {
+      --local-icon-fill: var(--woly-canvas-text-default);
+    }
+  }
+
+  &[data-disabled='true'] {
+    pointer-events: none;
+
+    [data-icon] {
+      --local-icon-fill: var(--woly-canvas-text-disabled);
+    }
+    
+    [data-selected] {
+      --local-background-input-color: var(--woly-canvas-disabled);
+      --local-border-input-color: var(--woly-canvas-text-disabled);
+      --local-value-color: var(--woly-canvas-text-disabled);
+    }
   }
 ` as StyledComponent<'div', Record<string, unknown>, SelectProps & Variant>;
