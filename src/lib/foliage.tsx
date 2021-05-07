beaa02298799df55ff2407af21e9332e1271b839
Sergey Sova <mail@sergeysova.com>
2021-05-07 14:58:39 +0300

    feat: initially add foliage api

--- src/lib/foliage.tsx
+++ src/lib/foliage.tsx
@@ -3,6 +3,8 @@ import styled, { Interpolation, css } from 'styled-components';
 
 // type DOMTag = keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap;
 
+export { css };
+
 interface Variant<Props> {
   [caseName: string]: Interpolation<Props>;
 }
@@ -18,7 +20,7 @@ type DOMProperties<Tag extends DOMTag> = keyof JSX.IntrinsicElements[Tag];
 
 interface Config<Tag extends DOMTag, Props, Variants extends Record<string, Variant<Props>>> {
   // mapVariants
-  attrs?: Partial<DOMProperties<Tag>>;
+  attrs?: Partial<JSX.IntrinsicElements[Tag]>;
   variants?: Variants;
   defaults?: { [VariantKey in keyof Variants]?: keyof Variants[VariantKey] };
   compound?: Array<
@@ -29,6 +31,10 @@ interface Config<Tag extends DOMTag, Props, Variants extends Record<string, Vari
   children?: React.FC<Props>;
 }
 
+type PropsFromVariants<Props, Variants extends Record<string, Variant<Props>>> = {
+  [Key in keyof Variants]: keyof Variants[Key];
+};
+
 export function component<
   Tag extends DOMTag,
   Props extends {},
@@ -37,7 +43,7 @@ export function component<
   tag: Tag,
   content: Interpolation<Props> | Interpolation<Props>[],
   config: Config<Tag, Props, Variants> = {},
-): React.FC<Props> {
+): React.FC<Props & PropsFromVariants<Props, Variants>> {
   const {
     defaults = {},
     variants = {},
@@ -68,43 +74,57 @@ export function component<
     cssChunks.push(`${selector.join('')} { ${css} }`);
   }
 
-  const d = styled[tag];
-  d;
+  const creator = attrs ? styled(tag).attrs(attrs as any) : styled(tag);
+  const Component = creator`${cssChunks.join('')}`;
 
-  // const tagLiteral = attrs ? styled[tag].attrs(attrs) : styled[tag];
-
-  return () => null as any;
+  return (props) => {
+    const variantsData: Record<string, string> = {};
+    for (const [key, cases] of Object.entries(variants as Variants)) {
+      if (key in props && props[key] in cases) {
+        const data = `data-${key}`;
+        variantsData[data] = String(props[key]);
+      }
+    }
+    for (const [variant, defaultCase] of Object.entries(defaults)) {
+      if (!(variant in props)) {
+        variantsData[`data-${variant}`] = defaultCase as string;
+      }
+    }
+    return React.createElement(Component, { ...props, ...variantsData });
+  };
 }
 
-const Demo = component(
-  'main',
-  css`
-    color: black;
-  `,
-  {
-    // mapVariants: { demo: (count) => count > 0 ? 'demo' : 'second' },
-    variants: {
-      demo: {
-        first: css`
-          width: 20px;
-        `,
-        second: css`
-          width: 10px;
-        `,
+if (0) {
+  const Demo = component(
+    'main',
+    css`
+      color: black;
+    `,
+    {
+      // mapVariants: { demo: (count) => count > 0 ? 'demo' : 'second' },
+      variants: {
+        demo: {
+          first: css`
+            width: 20px;
+          `,
+          second: css`
+            width: 10px;
+          `,
+        },
+        size: {
+          big: css``,
+          small: css``,
+        },
       },
-      size: {
-        big: css``,
-        small: css``,
-      },
+      defaults: { demo: 'first' },
+      attrs: { tabIndex: 1 },
+      compound: [
+        {
+          css: css``,
+          demo: 'first',
+          size: 'small',
+        },
+      ],
     },
-    defaults: { demo: 'first' },
-    attrs: { tabIndex: 0 },
-    compound: [
-      {
-        css: css``,
-        demo: 'first',
-        size: 'small',
-      },
-    ],
-  },
-);
+  );
+}
--- src/ui/atoms/button/index.tsx
+++ src/ui/atoms/button/index.tsx
@@ -2,6 +2,103 @@
 import * as React from 'react';
 import styled, { StyledComponent } from 'styled-components';
 import { Variant } from 'lib/types';
+import { component, css } from 'lib/foliage';
+
+const block = css`
+  --local-vertical: calc(1px * var(--woly-component-level) * var(--woly-main-level));
+  --local-horizontal: calc(
+    var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--local-vertical)
+  );
+  --local-gap: var(--local-vertical);
+  --local-compensate: var(--woly-const-m);
+`;
+
+const button = css`
+  --local-text-color: var(--woly-shape-text-default);
+  --local-shape-color: var(--woly-shape-default);
+  --local-border-color: var(--woly-shape-default);
+
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: nowrap;
+  padding: var(--local-vertical) 0;
+  justify-content: center;
+
+  color: var(--local-text-color);
+  font-size: var(--woly-font-size);
+  line-height: var(--woly-line-height);
+
+  background-color: var(--local-shape-color);
+  border-color: var(--local-border-color);
+  border-style: solid;
+  border-width: var(--woly-border-width);
+  border-radius: var(--woly-rounding);
+  outline: none;
+
+  [data-icon] {
+    --local-icon-size: var(--woly-line-height);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: var(--local-icon-size);
+    height: var(--local-icon-size);
+    padding: 0 0 0 calc(var(--local-horizontal) - var(--local-compensate));
+  }
+
+  [data-text] {
+    padding: 0 var(--local-horizontal);
+  }
+
+  span[data-icon='left'] + span[data-text] {
+    padding-left: var(--local-gap);
+  }
+
+  svg > path {
+    fill: var(--local-text-color);
+  }
+
+  &:hover {
+    --local-text-color: var(--woly-shape-text-hover);
+    --local-border-color: var(--woly-shape-hover);
+    --local-shape-color: var(--woly-shape-hover);
+  }
+
+  &:active {
+    --local-text-color: var(--woly-shape-text-active);
+    --local-border-color: var(--woly-shape-active);
+    --local-shape-color: var(--woly-shape-active);
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 1.5px var(--woly-focus);
+  }
+
+  &:disabled {
+    --local-text-color: var(--woly-shape-text-disabled);
+    --local-border-color: var(--woly-shape-disabled);
+    --local-shape-color: var(--woly-shape-disabled);
+  }
+`;
+
+export const Button = component('button', [block, button], {
+  variants: {
+    defaults: { outlined: 'false' },
+    outlined: {
+      true: css`
+        background-color: transparent;
+        color: var(--local-shape-color);
+        svg > path {
+          fill: var(--local-shape-color);
+        }
+      `,
+      false: css``,
+    },
+    variant: {
+      primary: css``,
+      secondary: css``,
+    },
+  },
+});
 
 export type ButtonVariants = 'default' | 'primary' | 'destructive' | 'text';
 export type ButtonSizes = 'default' | 'small';
@@ -28,43 +125,7 @@ const ButtonBase: React.FC<ButtonProps & Variant> = ({
   </button>
 );
 
-export const Button = styled(ButtonBase)`
-  --local-vertical: calc(1px * var(--woly-component-level) * var(--woly-main-level));
-  --local-horizontal: calc(
-    var(--woly-const-m) + (1px * var(--woly-main-level)) + var(--local-vertical)
-  );
-  --local-gap: var(--local-vertical);
-  --local-compensate: var(--woly-const-m);
-
-  --local-text-color: var(--woly-shape-text-default);
-  --local-shape-color: var(--woly-shape-default);
-  --local-border-color: var(--woly-shape-default);
-
-  box-sizing: border-box;
-  display: flex;
-  flex-wrap: nowrap;
-  padding: var(--local-vertical) 0;
-  justify-content: center;
-
-  color: var(--local-text-color);
-  font-size: var(--woly-font-size);
-  line-height: var(--woly-line-height);
-
-  background-color: var(--local-shape-color);
-  border-color: var(--local-border-color);
-  border-style: solid;
-  border-width: var(--woly-border-width);
-  border-radius: var(--woly-rounding);
-  outline: none;
-
-  &[data-outlined='true'] {
-    background-color: transparent;
-    color: var(--local-shape-color);
-    svg > path {
-      fill: var(--local-shape-color);
-    }
-  }
-
+export const Button_ = styled(ButtonBase)`
   [data-icon] {
     --local-icon-size: var(--woly-line-height);
     display: flex;
