4e2c163731c4f1f7dcc703375cb84e6554cffee5
Ira <i.aristova@opends.tech>
2021-05-28 12:59:56 +0300

    add comments

--- src/ui/molecules/select/index.tsx
+++ src/ui/molecules/select/index.tsx
@@ -26,7 +26,8 @@ interface SelectProps {
   placeholder?: string;
   selected: string | null;
 }
-
+// in the future, should to set the number of items in the list
+// and decide whether the select will contain a scroll and something else
 export const SelectBase: React.FC<SelectProps & Variant> = ({
   className,
   disabled = false,
