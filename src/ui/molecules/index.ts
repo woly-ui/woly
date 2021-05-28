b7586c5f6c6a083aeaa5a662f2790f89416ba080
Ira <i.aristova@opends.tech>
2021-05-28 19:19:34 +0300

    fix lint

--- src/ui/molecules/index.ts
+++ src/ui/molecules/index.ts
@@ -5,5 +5,4 @@ export { InputPassword } from './input-password';
 export { Modal } from './modal';
 export { RadioButton } from './radio-button';
 export { Popover } from './popover';
-export { Select } from './select';
 export { Switch } from './switch';
