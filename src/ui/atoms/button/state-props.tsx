e872977dff67787a3303fb1902c5a240c25233e6
Tatiana Cherednichenko <tatinacher@gmail.com>
2021-06-08 15:28:05 +0300

    feat: generate map of states

---
+++ src/ui/atoms/button/state-props.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import * as React from 'react';
+import { GenerateMap } from 'lib/map-generation';
+import { IconSearch } from 'static/icons';
+
+import { Button } from '.';
+
+interface ComponentProps {
+  name: string;
+  values: Array<string | boolean>;
+}
+
+interface ComponentButtonProps {
+  disabled: boolean;
+  icon: boolean;
+  outlined: boolean;
+  sizes: 'N' | 'XS' | 'S' | 'M' | 'L' | 'XL' | 'H';
+  variants: string;
+}
+
+export const variants: ComponentProps = {
+  name: 'variants',
+  values: ['primary', 'secondary'],
+};
+
+export const sizes: ComponentProps = {
+  name: 'sizes',
+  values: ['N', 'XS', 'S', 'M', 'L', 'XL', 'H'],
+};
+
+export const otherProps: Record<string, Array<unknown>> = {
+  disabled: [false, true],
+  outlined: [false, true],
+  icon: [false, true],
+};
+
+export const ComponentButton: React.FC<{ prop: ComponentButtonProps }> = ({ prop }) => (
+  <Button
+    disabled={prop.disabled}
+    icon={prop.icon ? <IconSearch /> : null}
+    onClick={() => console.log('Hi!')}
+    outlined={prop.outlined}
+    text="Button"
+    variant={prop.variants}
+  />
+);
+
+export const GenerateButton = () => (
+  <GenerateMap
+    component={ComponentButton}
+    otherProps={otherProps}
+    sizes={sizes}
+    variants={variants}
+  />
+);
