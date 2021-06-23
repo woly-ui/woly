53b06c341b93ba95e29a7e647961d3d7525936e1
Ira <i.aristova@opends.tech>
2021-06-23 21:26:12 +0300

    add weight to tab

--- src/woly/molecules/tabs/index.tsx
+++ src/woly/molecules/tabs/index.tsx
@@ -13,7 +13,7 @@ interface TabProps {
   active?: boolean;
   className?: string;
   href?: string;
-  outlined?: boolean;
+  weight?: string;
 }
 
 const mapTabs = (properties: Priority) => ({
@@ -44,36 +44,39 @@ const TabBase: React.FC<TabProps & TabElementProps & Priority> = ({
   iconLeft,
   iconAction,
   onClick,
-  outlined,
+  weight = '2',
   text,
-  priority
-}) => (
-  <div
-    className={className}
-    data-href={href}
-    data-outlined={outlined}
-    onClick={onClick}
-    tabIndex={0}
-    data-active={active}
-    data-priority={priority}
-  >
-    {iconLeft && <span data-icon="link-icon">{iconLeft}</span>}
-    <span data-link="link-text">{text}</span>
-    {iconAction && <span data-icon="tab-action">{iconAction}</span>}
-  </div>
-);
+  priority,
+}) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      className={className}
+      data-href={href}
+      data-weight={weight}
+      onClick={onClick}
+      tabIndex={0}
+      data-active={active}
+      data-priority={priority}
+    >
+      {iconLeft && <span data-icon="link-icon">{iconLeft}</span>}
+      <span data-link="link-text">{text}</span>
+      {iconAction && <span data-icon="tab-action">{iconAction}</span>}
+    </div>
+  );
+};
 
 export const Tab = styled(TabBase)`
-  --local-icon-color: var(--woly-canvas-text-default);
-  --local-background: var(--woly-canvas-disabled);
-  --local-color: var(--woly-canvas-text-default);
   --local-icon-size: var(--woly-line-height);
-  --local-border-color: var(--woly-shape-default);
   --local-tab-max-size: 201px;
   --local-tab-min-size: 67px;
-  --local-gradient-color-default: linear-gradient(to right, rgba(255, 255, 255, 0.2), var(--local-background) 100%);
-  --local-gradient-color-hover: linear-gradient(to right,rgba(255, 255, 255, 0.2), var(--woly-background) 100%);
-  --local-gradient-color-active: linear-gradient(to right,rgba(255, 255, 255, 0.2), var(--woly-background) 100%);
+
+  --local-gradient-color: linear-gradient(
+    45deg,
+    rgba(var(--woly-shape-default), 0.2),
+    var(--woly-shape-default) 100%
+  );
 
   ${box}
 
@@ -85,14 +88,13 @@ export const Tab = styled(TabBase)`
   min-width: var(--local-tab-min-size);
   max-width: var(--local-tab-max-size);
 
-  color: var(--local-color);
   font-size: var(--woly-font-size);
   line-height: var(--woly-line-height);
 
   text-decoration: none;
 
-  background-color: var(--local-background);
-  border-right: var(--woly-border-width) solid var(--local-border-color);
+  border-right-width: var(--woly-border-width);
+  border-right-style: solid;
 
   outline: none;
 
@@ -105,18 +107,15 @@ export const Tab = styled(TabBase)`
     overflow: hidden;
 
     white-space: nowrap;
-
-    background-color: var(--local-background);
+    text-align: center;
 
     &::after {
       position: absolute;
       top: 0;
       right: 0;
 
-      width: 35%;
+      width: 20%;
       height: 100%;
-
-      background: var(--local-gradient-color);
 
       content: '';
     }
@@ -136,33 +135,6 @@ export const Tab = styled(TabBase)`
     svg {
       width: 100%;
       height: 100%;
-
-      path {
-        fill: var(--local-icon-color);
-      }
-    }
-  }
-
-  &[data-outlined='true'] {
-    color: var(--local-shape-color);
-
-    background-color: transparent;
-
-    [data-icon] > svg {
-      path {
-        fill: var(--local-shape-color);
-      }
-    }
-  }
-
-  &:hover {
-    --local-background: var(--woly-canvas-default);
-    [data-link='link-text'] {
-      --local-background: var(--woly-canvas-default);
-
-      &::after {
-        --local-gradient-color: var(--local-gradient-color-hover);
-      }
     }
   }
 
@@ -171,18 +143,138 @@ export const Tab = styled(TabBase)`
 
     box-shadow: 0 var(--woly-border-width) 0 0 var(--woly-focus-color);
 
-    --local-background: var(--woly-canvas-default);
-    [data-link='link-text'] {
-      &::after {
-        --local-gradient-color: var(--local-gradient-color-active);
-      }
+    &:hover,
+    &:focus-within {
+      background: var(--woly-background);
     }
   }
 
-  &:focus {
-    z-index: 1;
+  &[data-weight='1'] {
+    --local-text-color: var(--woly-canvas-text-default);
+    color: var(--local-text-color);
 
-    outline: none;
-    box-shadow: 0 var(--woly-border-width) 0 0 var(--woly-focus-color);
+    background: transparent;
+    border-right-color: transparent;
+
+    &:hover {
+      --local-text-color: var(--woly-shape-hover);
+    }
+
+    &:focus-within {
+      --local-text-color: var(--woly-shape-active);
+      box-shadow: 0 var(--woly-border-width) 0 0 var(--woly-focus-color);
+    }
+
+    &[data-active='true'] {
+      --local-text-color: var(--woly-focus-color);
+    }
+  }
+
+  &[data-weight='2'] {
+    --local-text-color: var(--woly-shape-default);
+    --local-background: var(--woly-shape-disabled);
+    --local-border-color: var(--woly-shape-default);
+
+    color: var(--local-text-color);
+
+    background: var(--local-background);
+    border-right-color: var(--local-border-color);
+
+    [data-icon='link-icon'] > svg > path {
+      fill: var(--local-text-color);
+    }
+
+    [data-link='link-text'] {
+      &::after {
+        --local-gradient-color: linear-gradient(
+          to right,
+          rgba(255, 255, 255, 0.2),
+          var(--woly-shape-disabled) 100%
+        );
+        background: var(--local-gradient-color);
+      }
+    }
+
+    &[data-active='true'] {
+      --local-background: var(--woly-background);
+      --local-text-color: var(--woly-canvas-text-active);
+
+      [data-link='link-text'] {
+        --local-background: var(--woly-background);
+        &::after {
+          --local-gradient-color: linear-gradient(
+            to right,
+            rgba(255, 255, 255, 0.2),
+            var(--woly-background) 100%
+          );
+        }
+      }
+    }
+
+    &:hover {
+      --local-background: transparent;
+      --local-text-color: var(--woly-shape-hover);
+      --local-border-color: var(--woly-shape-active);
+      [data-link='link-text'] {
+        &::after {
+          --local-gradient-color: linear-gradient(
+            to right,
+            rgba(255, 255, 255, 0.2),
+            var(--woly-background) 100%
+          );
+        }
+      }
+
+      &:focus-within {
+        z-index: 1;
+
+        outline: none;
+        box-shadow: 0 var(--woly-border-width) 0 0 var(--woly-focus-color);
+        --local-gradient-color: linear-gradient(
+          45deg,
+          rgba(255, 255, 255, 0.2),
+          var(--woly-background) 100%
+        );
+      }
+    }
+
+    &[data-weight='3'] {
+      --local-background: var(--woly-shape-default);
+      --local-text-color: var(--woly-shape-text-default);
+      --local-border-color: var(--woly-shape-default);
+
+      color: var(--local-text-color);
+
+      background-color: var(--local-background);
+      border-right-color: var(--local-border-color);
+
+      [data-link='link-text'] {
+        background-color: var(--local-background);
+
+        &::after {
+          background: var(--local-gradient-color);
+        }
+      }
+
+      [data-icon='link-icon'] > svg > path {
+        fill: var(--local-text-color);
+      }
+
+      &:hover {
+        --local-background: var(--woly-shape-hover);
+      }
+
+      &:focus-within {
+        z-index: 1;
+
+        outline: none;
+        box-shadow: 0 var(--woly-border-width) 0 0 var(--woly-focus-color);
+      }
+
+      &[data-active='true'] {
+        --local-text-color: var(--woly-canvas-text-active);
+        --local-background: var(--woly-background);
+      }
+    }
   }
 ` as StyledComponent<'div', Record<string, unknown>, TabProps & TabElementProps & Priority>;
