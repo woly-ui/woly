33ecf5276352d3ac7de8c4cf8f5affe0ebcc8549
Шараев Айнур Раильевич <mr.sharaevainur@yandex.ru>
2021-07-14 14:05:31 +0300

    refactor: add comments

--- src/woly/molecules/modal/use-scroll-lock.ts
+++ src/woly/molecules/modal/use-scroll-lock.ts
@@ -1,11 +1,17 @@
 import React from 'react';
 
 interface ScrollLockOptions {
+  /**
+   * If the overflow property of the body is set to hidden, the body widens by the width of the scrollbar (only on some OS, like Windows). This produces an unpleasant flickering effect, especially on websites with centered content. If the reserveScrollBarGap option is set, this gap is filled by a padding-right on the body element. If enableBodyScroll is called, the padding-right is automatically reset to the previous value.
+   */
   reserveScrollBarGap?: boolean;
 }
 
 export const useScrollLock = (options: ScrollLockOptions = { reserveScrollBarGap: false }) => {
   const [lock, setLock] = React.useState<HTMLElement | null>(null);
+  /**
+   * Body could have different padding and overflow settings and they are saved prior disabling body's scroll. If enableBodyScroll is called, these styles are automatically reset to the previous values
+   */
   const [previousBodyOverflowSetting, setPreviousBodyOverflowSetting] = React.useState<
     string | null
   >(null);
