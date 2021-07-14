ecbcccc51dd83e9e2c961c007cb8fcbd02f3c737
Шараев Айнур Раильевич <mr.sharaevainur@yandex.ru>
2021-07-14 14:15:47 +0300

    fix: correct formula

--- src/woly/elements/box/index.tsx
+++ src/woly/elements/box/index.tsx
@@ -34,7 +34,7 @@ export const box = css`
 `;
 
 export const boxInline = css`
-  --local-vertical: calc(1px * var(--woly-component-level) * var(--woly-main-level) / 2);
+  --local-vertical: calc(1px * var(--woly-component-level) * var(--woly-main-level));
 
   & > * {
     padding-top: var(--local-vertical);
