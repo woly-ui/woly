4f38c56c01274f437ce968ac7cbf159b308251af
risenforces <risenforces@gmail.com>
2021-07-19 15:18:49 +0300

    fix(keyboard-event): types

--- src/lib/keyboard-event.ts
+++ src/lib/keyboard-event.ts
@@ -2,25 +2,29 @@ import React from 'react';
 
 interface KeyboardEventProps {
   event: React.KeyboardEvent;
-  keyHandler?: any;
-  shiftKeyHandler?: any;
+  keyHandler?: Handler;
+  shiftKeyHandler?: Handler;
 }
 
-// interface HandlerType {
-//   [key: string]: (event: any) => void;
-// }
+interface Handler {
+  [key: string]: (event: React.KeyboardEvent) => void;
+}
 
 const camelCase = (string: string) => string.charAt(0).toLowerCase() + string.slice(1);
+
+const hasHandler = (handler: Handler, key: string) => {
+  return Object.prototype.hasOwnProperty.call(handler, key);
+};
 
 export const keyboardEventHandle = ({ event, keyHandler, shiftKeyHandler }: KeyboardEventProps) => {
   const { shiftKey } = event;
   const key = camelCase(event.key);
 
-  if (key && shiftKey && shiftKeyHandler && shiftKeyHandler.hasOwnProperty(key)) {
+  if (key && shiftKey && shiftKeyHandler && hasHandler(shiftKeyHandler, key)) {
     shiftKeyHandler[key](event);
   }
 
-  if (key && keyHandler && keyHandler.hasOwnProperty(key)) {
+  if (key && keyHandler && hasHandler(keyHandler, key)) {
     keyHandler[key](event);
   }
 };
